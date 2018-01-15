318f6363bdfe742956ce55f12fc069b56c86a85d
Matthew Potter <heymatthewoden@gmail.com>
2018-01-15 13:16:38 -0600

    add parens to silence warnings

--- mix.exs
+++ mix.exs
@@ -2,11 +2,13 @@ defmodule Proxy.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :proxy,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     deps: deps,
-     aliases: aliases]
+    [
+      app: :proxy,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      deps: deps(),
+      aliases: aliases()
+     ]
   end
 
   # Some command line aliases
