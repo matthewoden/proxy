04b273f51ef7ed90275be58b5327fc094d3e68e4
Matthew Potter <heymatthewoden@gmail.com>
2018-01-15 11:17:21 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Proxy.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:cowboy, "~> 1.0.0"},
+    [{:cowboy, "~> 1.1.2"},
      {:plug, "~> 0.11.1"},
      {:hackney, "~> 1.10.0"}]
   end
